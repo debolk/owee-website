d3d2a7f87e799e1c21d0f65aa375c8516183d027
Alchemi1963 <alchemi1999@hotmail.com>
2024-07-15 18:11:26 +0200

    Added multi-language support

--- public/js/include.js
+++ public/js/include.js
@@ -1,9 +1,18 @@
 function includeHTML(){
   let elements = document.getElementsByTagName("include-html");
+  
+  let lang = 'nl';
+  if(document.cookie !== '') {
+	let cookie = JSON.parse(document.cookie.split(';')[0]);
+	lang = cookie.lang;
+  } else {
+	document.cookie = JSON.stringify({lang: 'nl'}) + '; SameSite=Strict; Secure; expires=' + getCookieExpire();
+	console.log(JSON.stringify({lang: 'nl'}) + '; SameSite=Strict; Secure; expires=' + getCookieExpire());
+  }
+  
   for (let e of elements){
     if (e.hasAttribute("src")) {
-
-      fetch(e.getAttribute("src"))
+      fetch(`./html/${lang}/${e.getAttribute("src")}`)
         .then((res) => res.text())
         .then((html) => {
           let v = document.createElement("div");
@@ -16,3 +25,15 @@ function includeHTML(){
     }
   }
 }
+
+function getCookieExpire() {
+	let expires = new Date();
+	expires.setTime(expires.getTime() + 1000 * 86400 * 5);
+	return expires.toUTCString();
+}
+
+function changeToLang(lang){
+	document.cookie = JSON.stringify({lang: `${lang}`}) + '; SameSite=Strict; Secure; expires=' + getCookieExpire();
+	location.reload();
+}
+	
